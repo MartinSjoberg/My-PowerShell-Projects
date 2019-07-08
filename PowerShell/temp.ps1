89312cbf50cec94b97d12a7b23c432a1fa6732e6
Martin Sjöberg <guldhjartats@hotmail.com>
2019-07-08 13:45:24 +0200

    Update temp.ps1

--- PowerShell/temp.ps1
+++ PowerShell/temp.ps1
@@ -1,13 +1,7 @@
 # Connect vcenter  SNAPSHOT 2
-Connect-VIServer vcenter01.vt.net
+Connect-VIServer vcenter
 
 # Skapa snapshot
-New-Snapshot -VM server1412 -Name MSSnap2 -RunAsync -Description 'MS took this snapshot 2'
-New-Snapshot -VM server1413 -Name MSSnap2 -RunAsync -Description 'MS took this snapshot 2'
-New-Snapshot -VM server1414 -Name MSSnap2 -RunAsync -Description 'MS took this snapshot 2'
-New-Snapshot -VM server1415 -Name MSSnap2 -RunAsync -Description 'MS took this snapshot 2'
-New-Snapshot -VM server1416 -Name MSSnap2 -RunAsync -Description 'MS took this snapshot 2'
-New-Snapshot -VM server1417 -Name MSSnap2 -RunAsync -Description 'MS took this snapshot 2'
-New-Snapshot -VM server1418 -Name MSSnap2 -RunAsync -Description 'MS took this snapshot 2'
+New-Snapshot -VM server -Name MSSnap2 -RunAsync -Description 'MS took this snapshot'
 
 Disconnect-VIServer
