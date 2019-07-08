f0ccaf1d4ff9e1b49c7e8c9c21bf238762079f4f
Martin Sjöberg <guldhjartats@hotmail.com>
2019-07-08 15:23:45 +0200

    Update FindDisksLost.ps1

--- PowerShell/FindDisksLost.ps1
+++ PowerShell/FindDisksLost.ps1
@@ -2,7 +2,7 @@
 Get-Eventlog -log System -computername server1 -Message "*has been surprise removed.*" | Out-GridView
 
 # One server faster!
-Get-WinEvent -computername server1 -FilterHashTable @{ LogName = "System"; ID = 157 }
+Get-WinEvent -computername server1 -FilterHashTable @{ LogName = "System"; ID = 157 } | Out-GridView
 
 # Multiple servers
 
